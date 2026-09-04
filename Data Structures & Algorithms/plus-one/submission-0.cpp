14c724ba41fe098f839d6614097ac12021d6d761
alankritamingwal7-afk <alankritamingwal7@gmail.com>
2026-09-04 18:02:31 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int end = digits.size()-1;
+        for(int i=end; i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits; 
+    }
+};
