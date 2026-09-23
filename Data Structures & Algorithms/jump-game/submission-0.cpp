8df7062b6392f77472dd9383e15e3f540449314f
alankritamingwal7-afk <alankritamingwal7@gmail.com>
2026-09-23 22:13:32 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int max_idx=0;
+        for(int i=0;i<n;i++){
+            if(i>max_idx){
+                return false;
+            }
+            max_idx=max(max_idx,i+nums[i]);
+            
+        }
+        return true;
+    }
+};
