94e881a60414dd3d15810389b76580bd423cd0da
alankritamingwal7-afk <alankritamingwal7@gmail.com>
2026-09-25 19:19:38 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int INF = 1e9;
+        vector<int> dp(amount + 1, INF);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] = min(dp[j], dp[j - coin] + 1);
+            }
+        }
+
+        return dp[amount] == INF ? -1 : dp[amount];
+    }
+};
